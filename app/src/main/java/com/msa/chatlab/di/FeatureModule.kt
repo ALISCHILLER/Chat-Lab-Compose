3c0d255d2fa91ba95c39f840d301f23c71d123b6
SOFTWARE-SV\zar <zartablet2022@gmail.com>
2026-02-24 14:29:55 +0330

    di

--- app/src/main/java/com/msa/chatlab/di/FeatureModule.kt
+++ app/src/main/java/com/msa/chatlab/di/FeatureModule.kt
@@ -3,31 +3,28 @@ package com.msa.chatlab.di
 import com.msa.chatlab.feature.chat.di.FeatureChatModule
 import com.msa.chatlab.feature.chat.domain.usecase.GetChatUiStateUseCase
 import com.msa.chatlab.feature.chat.domain.usecase.SendMessageUseCase
+import com.msa.chatlab.feature.chat.vm.ChatThreadViewModel
 import com.msa.chatlab.feature.chat.vm.ChatViewModel
-// import com.msa.chatlab.feature.chat.vm.ChatThreadViewModel
-// import com.msa.chatlab.feature.chat.vm.OutboxViewModel
-// import com.msa.chatlab.feature.connect.vm.ConnectViewModel
-// import com.msa.chatlab.feature.debug.vm.DebugViewModel
-// import com.msa.chatlab.feature.lab.vm.LabViewModel
-// import com.msa.chatlab.feature.settings.vm.SettingsViewModel
-import com.msa.chatlab.vm.SnackbarViewModel
+import com.msa.chatlab.feature.chat.vm.OutboxViewModel
+import com.msa.chatlab.feature.connect.vm.ConnectViewModel
+import com.msa.chatlab.feature.debug.vm.DebugViewModel
+import com.msa.chatlab.feature.lab.vm.LabViewModel
+import com.msa.chatlab.feature.settings.di.settingsModule
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
-
+import com.msa.chatlab.vm.SnackbarViewModel
 val FeatureModule = module {
-    includes(FeatureChatModule)
+    includes(FeatureChatModule, settingsModule)
 
     // --- Chat Feature ---
     factory { GetChatUiStateUseCase(get(), get(), get()) }
     factory { SendMessageUseCase(get()) }
     viewModel { ChatViewModel(get(), get(), get()) }
-
-    // --- Other Features (commented out as in original) ---
-    // viewModel { ChatThreadViewModel(get(), get(), get()) }
-    // viewModel { OutboxViewModel(get(), get()) }
-    // viewModel { ConnectViewModel(get(), get(), get()) }
-    // viewModel { SettingsViewModel(get(), get(), get(), get()) }
-    // viewModel { LabViewModel(get(), get()) }
-    // viewModel { DebugViewModel(get(), get(), get(), get()) }
+    viewModel { ChatThreadViewModel(get(), get(), get()) }
+    viewModel { OutboxViewModel(get(), get()) }
+    // --- Other Features ---
+    viewModel { ConnectViewModel(get(), get(), get()) }
+    viewModel { LabViewModel(get(), get()) }
+    viewModel { DebugViewModel(get(), get(), get(), get()) }
     viewModel { SnackbarViewModel() }
 }
